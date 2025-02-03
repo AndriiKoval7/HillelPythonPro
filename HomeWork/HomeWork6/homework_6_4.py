675fd77243b75b4272c3bf0800d1922a58d02d1f
AndriiKoval7 <andrii.koval@gmail.com>
2025-02-03 20:21:10 +0200

    homework_1_18.01.25

---
+++ HomeWork/HomeWork6/homework_6_4.py
@@ -0,0 +1,41 @@
+import json
+import os
+
+
+BOOKS_FILE = 'books.json'
+
+def load_books():
+    if os.path.exists(BOOKS_FILE):
+        with open(BOOKS_FILE, 'r', encoding='utf-8') as file:
+            return json.load(file)
+    return []
+
+def show_books():
+    books = load_books()
+    if not books:
+        print(f'No books found')
+        return
+    print(f'\nBooks found: {len(books)}')
+    for ind, book in enumerate(books):
+        status = "in stock" if book["in stock"] else "not in stock"
+        print(f'{ind + 1}) {book["book_name"]}, author: {book["author"]}, {status}')
+
+def save_book(book):
+    with open(BOOKS_FILE, 'w', encoding='utf-8') as file:
+        json.dump(book, file, ensure_ascii=False, indent=4)
+
+def add_book(book_name, author, year, in_stock):
+    books = load_books()
+    books.append({
+        "book_name": book_name,
+        "author": author,
+        "year": year,
+        "in stock": in_stock
+    })
+    save_book(books)
+    print(f'Books added: {book_name}')
+
+
+show_books()
+add_book('book_n','author_n',2017,True)
+show_books()
